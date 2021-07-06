04ccbee347ae05e42dc76987a7e70c1a21bc8130
Irina-88 <schastie-88@bk.ru>
2021-07-06 18:07:38 +0200

    added script for open & close modal window

--- scripts/script.js
+++ scripts/script.js
@@ -6,4 +6,27 @@ headerCityButton.addEventListener('click', () => {
   const city = prompt('Укажите ваш город');
   headerCityButton.textContent = city;
   localStorage.setItem('lomoda-location', city);
-})
+});
+
+//модальное окно
+
+const subheaderСart = document.querySelector('.subheader__cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+
+const cartModalOpen = () => {
+  cartOverlay.classList.add('cart-overlay-open');
+};
+
+const cartModalClose = () => {
+  cartOverlay.classList.remove('cart-overlay-open');
+};
+
+subheaderСart.addEventListener('click', cartModalOpen);
+
+cartOverlay.addEventListener('click', event => {
+  const target = event.target;
+
+  if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
+    cartModalClose();
+  }
+});
